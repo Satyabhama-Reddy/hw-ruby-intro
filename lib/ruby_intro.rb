77d86d48720f606ac582c353e4a8136f4504e13a
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2022-09-09 14:08:25 -0500

    HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,61 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.length == 1
+
+  arr.sort.last(2).sum
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  return false if arr.empty? || arr.length == 1
+
+  arr.each_with_index do |ele_i, i|
+    arr[i + 1..].each do |ele_j|
+      return true if ele_i + ele_j == number
+    end
+  end
+
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  string.match(/^[b-d f-h j-n p-t v-z B-D F-H J-N P-T V-Z]/)
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  return true if string == '0'
+  return false if string.size < 3 || string.count('01') != string.size
+  return true if string[-1] == '0' && string[-2] == '0'
+
+  false
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError.new("Invalid isbn provided, got #{isbn}") if isbn.nil? || isbn.empty?
+    raise ArgumentError.new("Price must be a positive number, got #{price}") if price.nil? || price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{format('%.2f', @price)}"
+  end
 end
